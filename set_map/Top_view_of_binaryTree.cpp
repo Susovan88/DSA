2876d1454827e15d0f80da732e4c2b24efe13c07
Susovan Paul <palsusovan88@gmail.com>
2025-12-25 15:20:39 +0530

    top view of binary tree solve useing queue and level order traversal

---
+++ set_map/Top_view_of_binaryTree.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<unordered_map>
+#include<unordered_set>
+#include <vector>
+#include<queue>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+
+vector<int> topView(Node *root) {
+    // code here
+    vector<int>arr;
+    unordered_map<int,Node*>mp;
+    queue<Node*>qu1;
+    queue<int>qu2;
+    
+    qu1.push(root);
+    qu2.push(0);
+    mp.insert({0,root});
+    
+    Node* temp=NULL;
+    int a;
+    int min_level=0;  // min index of vertical level 
+    
+    while(qu1.size()!=0){
+        temp=qu1.front();
+        a=qu2.front();
+        qu1.pop(); qu2.pop();
+        
+        if(mp.find(a)==mp.end()){
+            mp[a]=temp;
+            min_level=min(min_level,a);
+        }
+        
+        if(temp->left){ // left node
+            qu1.push(temp->left);
+            qu2.push(a-1);
+        }
+        
+        if(temp->right){  // right node
+            qu1.push(temp->right);
+            qu2.push(a+1);
+        }
+    }
+    
+    while(mp.find(min_level)!=mp.end()){
+        arr.push_back(mp[min_level++]->data);
+    }
+    
+    return arr;
+}
+
+
+int main() {
+    /*
+            1
+           / \
+          2   3
+           \
+            4
+             \
+              5
+               \
+                6
+
+        Top View: 2 1 3 6
+    */
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->right = new Node(4);
+    root->left->right->right = new Node(5);
+    root->left->right->right->right = new Node(6);
+
+    vector<int> result = topView(root);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
