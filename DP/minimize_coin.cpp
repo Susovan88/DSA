362b0fde82308052afe01ebfd261541d229ecc42
Susovan Paul <palsusovan88@gmail.com>
2026-01-07 13:06:52 +0530

    minimize the coins solved.

---
+++ DP/minimize_coin.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<unordered_map>
+#include<unordered_set>
+#include<vector>
+#include<queue>
+#include<stack>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int miniMizeCoin(vector<int>&coin,vector<int>&dp,int n){
+    if(n==0) return 0;
+    if(dp[n]!=-2) return dp[n];
+
+    int minC=INT_MAX;
+    int c;
+    for(int i=0;i<coin.size();i++){
+        if(n>=coin[i]){
+            c=miniMizeCoin(coin,dp,n-coin[i]);
+            minC=min(minC,c);
+        }
+    }
+    return dp[n]=(minC==INT_MAX? INT_MAX:minC+1);
+}
+
+int coinChange(vector<int>& coins, int amount) {
+    if(amount==0) return 0;
+    vector<int>dp(amount+1,-2);
+    int count=miniMizeCoin(coins,dp,amount);
+    return count==INT_MAX? -1:count;
+}
+
+
+int main(){
+    int n,x;
+    cin>>n>>x;
+    vector<int>coins;
+    for(int i=0;i<n;i++){
+        int num;
+        cin>>num;
+        coins.push_back(num);
+    }
+
+    cout<<coinChange(coins,x);
+
+}
