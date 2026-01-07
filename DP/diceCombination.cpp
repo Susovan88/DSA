6587833119f0c161ab5d710d1548a508e06a0bab
Susovan Paul <palsusovan88@gmail.com>
2026-01-07 13:43:34 +0530

    bottom up solution of dce combination.

--- DP/diceCombination.cpp
+++ DP/diceCombination.cpp
@@ -24,6 +24,23 @@ int diceCom(int n){
     return dp[n]=sum%mod;
 }
 
+int diceCom_booto_up(int n){
+    dp.clear();
+    dp.resize(1000006,0);
+    dp[0]=1;
+
+    // f(n)=f(n-1)+f(n-2)+...+f(n-6)
+    for (int i = 1; i <= n; i++) {
+        for (int dice = 1; dice <= 6; dice++) {
+            if (i - dice >= 0) {
+                dp[i] = (dp[i] + dp[i - dice]) % mod;
+            }
+        }
+    }
+    
+    return dp[n];
+}
+
 int main(){
     dp.clear();
     dp.resize(1000006,-1);
