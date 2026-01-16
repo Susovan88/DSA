8bf901f778f890c5fc854eef660e45a1775bb167
Susovan Paul <palsusovan88@gmail.com>
2026-01-16 20:43:46 +0530

    min path sum -> solve again using bottom up

--- DP/min_path_sum.cpp
+++ DP/min_path_sum.cpp
@@ -20,6 +20,27 @@ int minPathSum(vector<vector<int>>& grid) {
     return helper(grid,minSum,0,0,m,n);
 }
 
+// bottom up solution
+int minPathSumBU(vector<vector<int>>& grid) {
+    int m=grid.size(),n=grid[0].size();
+    vector<vector<int>>dp(m,vector<int>(n,0));
+    // base case -> bottom up solution
+    dp[m-1][n-1]=grid[m-1][n-1];
+    for(int i=m-2;i>=0;i--){
+        dp[i][n-1]=grid[i][n-1]+dp[i+1][n-1];  // next right + current 
+    }
+    for(int i=n-2;i>=0;i--){
+        dp[m-1][i]=grid[m-1][i] + dp[m-1][i+1];  // next down + current 
+    }
+    // now apply this logic -> dp[i,j]=dp[i+1,j]+dp[i,j+1]
+    for(int l=m-2;l>=0;l--){
+        for(int r=n-2;r>=0;r--){
+            dp[l][r]=grid[l][r]+min(dp[l+1][r],dp[l][r+1]);
+        }
+    }
+    return dp[0][0];
+}
+
 int main() {
     vector<vector<int>> grid = {
         {1, 3, 1},
@@ -27,7 +48,7 @@ int main() {
         {4, 2, 1}
     };
 
-    cout <<minPathSum(grid);
-
+    cout <<minPathSum(grid)<<endl;
+    cout <<minPathSumBU(grid)<<endl;
     return 0;
 }
