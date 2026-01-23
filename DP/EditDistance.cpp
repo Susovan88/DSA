153144f4c2dad48afb4aba8182adb5e5c3da0cd4
Susovan Paul <palsusovan88@gmail.com>
2026-01-23 23:03:34 +0530

    solved Delete Operation for Two Strings

--- DP/EditDistance.cpp
+++ DP/EditDistance.cpp
@@ -32,9 +32,35 @@ public:
 };
 
 
+// 583. Delete Operation for Two Strings
+class Solution1 {
+public:
+    int dp[505][505];
+    int helper(string &word1, string &word2,int i,int j){
+        if(i==word1.size() && j==word2.size()) return 0;
+        if(i==word1.size()) return word2.size()-j;
+        if(j==word2.size()) return word1.size()-i;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(word1[i]==word2[j]) return helper(word1,word2,i+1,j+1);
+
+        int ans =INT_MAX;
+        ans=min(ans,helper(word1,word2,i+1,j)+1);
+        ans=min(ans,helper(word1,word2,i,j+1)+1);
+        return dp[i][j]=ans;
+    }
+    int minDistance(string word1, string word2) {
+        memset(dp,-1,sizeof dp);
+        return helper(word1,word2,0,0);
+    }
+};
+
+
 int main(){
     string s1="intention";
     string s2="execution";
     Solution sol;
-    cout<<sol.minDistance(s1,s2);
+    Solution1 sol1;
+    cout<<sol.minDistance(s1,s2)<<endl;
+    cout<<sol1.minDistance(s1,s2)<<endl;
 }
