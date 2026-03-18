0c787ce390162856ba55c0472e08a3427c33b0a1
Susovan Paul <palsusovan88@gmail.com>
2026-03-18 16:17:02 +0530

    two sum modification

--- arraysAndVectors/TwoSum.cpp
+++ arraysAndVectors/TwoSum.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
+#include<unordered_map>
+#include<unordered_set>
 #include<vector>
+#include<list>
+#include<queue>
+#include <bits/stdc++.h>
+#define ll long long int
 using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) { // O(n), extra space used .
+    int n=nums.size();
+    unordered_map<int,int>mp;
+    vector<int>ans;
+    for(int i=0;i<n;i++){
+        if(mp.find(target-nums[i])!=mp.end()){
+            ans.push_back(mp[target-nums[i]]);
+            ans.push_back(i);
+            return ans;
+        }
+        mp[nums[i]]=i;
+    }
+    return ans;
+}
+
 int main(){
   int sum;
   cout<<"Enter target : ";
@@ -15,11 +37,8 @@ int main(){
     cin>>x;
     v.push_back(x);
   }
-  for(int i=0;i<v.size()-1;i++){   //i<=v.size()-2
-    for(int j=i+1;j<v.size();j++){  //i<=v.size()-1
-      if(v[i]+v[j]==sum){
-        cout<<"("<<i<<","<<j<<")"<<endl;
-      }
-    }
+  vector<int> ans=twoSum(v,sum);
+  for(int i=0;i<ans.size();i++){  
+    cout<<ans[i]<<" ";
   }
 }
