fc62bbfbdecce88f2add178065d1ec9c4c5906c6
Susovan Paul <palsusovan88@gmail.com>
2026-04-03 10:26:52 +0530

    apply optimal approch using prefix ans sufix in Product of Array Except Self

--- arraysAndVectors/ProductOfArrayExceptSelf.cpp
+++ arraysAndVectors/ProductOfArrayExceptSelf.cpp
@@ -9,11 +9,31 @@
 using namespace std;
 
 // 238. Product of Array Except Self
-vector<int> productExceptSelf(vector<int>& nums) {
+vector<int> productExceptSelf2(vector<int> nums) {
+    int n=nums.size();
+    vector<int>pre(n);
+    vector<int>suf(n);
+    pre[0]=1;
+    for(int i=1;i<n;i++){ // prefix
+        pre[i]=pre[i-1]*nums[i-1];
+    }
+    suf[n-1]=1;
+    for(int i=n-2;i>=0;i--){ // sufix
+        suf[i]=suf[i+1]*nums[i+1];
+    }
+    for(int i=0;i<n;i++){   // -> prefix[i]* sufix[i]
+        nums[i]=suf[i]*pre[i];
+        cout<<pre[i]<<" - "<<suf[i]<<endl;
+    }
+    cout<<endl;
+    return nums;
+}
+
+
+vector<int> productExceptSelf(vector<int> nums) {
     int n=nums.size();
     int pro=1;
     int isZ=0;
-    int isN=0;
     for(int i=0;i<n;i++){
         if(nums[i]==0){
             isZ++;
@@ -33,7 +53,11 @@ vector<int> productExceptSelf(vector<int>& nums) {
 
 
 int main(){
-    vector<int>arr={1,2,43,-9,7,0,23,-7,4,5,-5,-12};
-    vector<int>ans=productExceptSelf(arr);
-    for(auto ele : arr)cout<<ele<<" ";
+    vector<int>arr={3,2,12,-9,7,0,6,-7,4,5,-5,-12};
+    vector<int>ans=productExceptSelf2(arr);
+    for(auto ele : ans)cout<<ele<<" ";
+
+    cout<<endl;
+    vector<int>ans2=productExceptSelf(arr);
+    for(auto ele : ans2)cout<<ele<<" ";
 }
