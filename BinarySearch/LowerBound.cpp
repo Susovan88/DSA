b455eee68a693e9990839241cf625c0c0fff486d
Susovan Paul <palsusovan88@gmail.com>
2026-04-04 00:18:13 +0530

    lower bound and upper bound of a elements in a array using binary search

--- BinarySearch/LowerBound.cpp
+++ BinarySearch/LowerBound.cpp
@@ -4,16 +4,32 @@
 #include <algorithm>
 using namespace std;
 
+/*
+✅ Lower Bound
+👉 First position where value is ≥ target
+
+✅ Upper Bound
+👉 First position where value is > target
+*/
 int main(){
-    vector<int> arr={4,6,12,34,43,54,66,70,88,90};
-    int t=80;
-    int st=0,end=arr.size()-1;
-    while(st<=end){
-        int mid=st+(end-st)/2;
-        if(arr[mid]==t) cout<<mid;
-        else if(arr[mid]<t) st=mid+1;
-        else end=mid-1;
+    vector<int> arr={4,6,12,34,34,34,34,43,54,54,54,54,70,70,88,90,100};
+    int t=34;
+    int st=0,end=arr.size(),mid;
+
+    // lower bound of t
+    while(st<end){
+        mid=st+(end-st)/2;
+        if(arr[mid]>=t)end=mid;
+        else st=mid+1;
     }
-    cout<<arr[st-1];
-    
+    cout<<"lower bound index is -> "<<st<<endl;
+
+    // upper bound of t
+    st=0,end=arr.size(),mid;
+    while(st<end){
+        mid=st+(end-st)/2;
+        if(arr[mid]<=t)st=mid+1;
+        else end=mid;
+    }
+    cout<<"upper bound index is -> "<<end<<" "<<arr[end]<<endl;
 }
