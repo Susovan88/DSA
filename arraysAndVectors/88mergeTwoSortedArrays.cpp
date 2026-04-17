56975e3940c2027216a31c150a132816be0e2599
Susovan Paul <palsusovan88@gmail.com>
2026-04-17 11:43:22 +0530

    solved 88. Merge Sorted Array

--- arraysAndVectors/88mergeTwoSortedArrays.cpp
+++ arraysAndVectors/88mergeTwoSortedArrays.cpp
@@ -2,6 +2,27 @@
 #include<vector>
 using namespace std;
 
+
+// 88. Merge Sorted Array
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) { // without use extra space , inplace
+    int i=m+n-1;
+    int x=m-1;
+    int y=n-1;
+    if(m==0)for(int i=0;i<n;i++)nums1[i]=nums2[i];
+    while(i>=0 && x>=0 && y>=0){
+        if(nums1[x]>nums2[y]){
+            nums1[i]=nums1[x];
+            if(x!=i)nums1[x]=0;
+            x--;
+        }else{
+            nums1[i]=nums2[y];
+            y--;
+        }
+        i--;
+    }
+    if(y>=0)for(int j=y;j>=0;j--)nums1[i--]=nums2[j];
+}
+
 void display(vector<int>& v3){
   for(int i=0;i<v3.size();i++){
         cout<<v3[i]<<" ";
@@ -9,7 +30,7 @@ void display(vector<int>& v3){
     cout<<endl;
 }
 
-vector<int> merge(vector<int>& v1,vector<int>& v2){
+vector<int> merge1(vector<int>& v1,vector<int>& v2){
   int n=v1.size(),m=v2.size();
   vector<int> v3(n+m);
   int i=0,j=0,k=0;   // i->v1  j->v2  k->v3
@@ -44,29 +65,8 @@ vector<int> merge(vector<int>& v1,vector<int>& v2){
 }
 
 int main(){
-  int n;
-  cout<<"Enter size of 1st array : ";
-  cin>>n;
-  vector<int>v1;
-  cout<<"Enter element :";
-  for(int i=0;i<n;i++){
-    int x;
-    cin>>x;
-    v1.push_back(x);
-  }
-
-  int m;
-  cout<<"Enter size of 2nd array : ";
-  cin>>m;
-  vector<int>v2;
-  cout<<"Enter element :";
-  for(int i=0;i<n;i++){
-    int x;
-    cin>>x;
-    v2.push_back(x);
-  }
-  vector<int> v3 = merge(v1,v2);
-
-  display(v3);
-
+  vector<int>arr1={1,4,6,9,14,16,17,0,0,0,0,0};
+  vector<int>arr2={2,4,7,11,18};
+  merge(arr1,7,arr2,5);
+  display(arr1);
 }
