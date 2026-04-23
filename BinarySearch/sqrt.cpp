938dad67fb9d82ea332e48c45e975af03992066f
Susovan Paul <palsusovan88@gmail.com>
2026-04-23 12:59:18 +0530

    solved nth root of m

--- BinarySearch/sqrt.cpp
+++ BinarySearch/sqrt.cpp
@@ -18,6 +18,37 @@ int mySqrt(int x) {
     return ans;
 }
 
+// https://www.geeksforgeeks.org/problems/square-root/1
+int floorSqrt(int n) {
+    if(sqrt(n)*sqrt(n)==n) return sqrt(n);
+    int st=0,end=n/2;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(mid*mid<=n)st=mid+1;
+        else end=mid-1;
+    }
+    return end;
+}
+
+// https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1
+int nthRoot(int n, int m) {
+    if(m==0 || m==1 || n==1) return m;
+    int st=1;
+    int end=m;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        long long val = 1;
+        for(int i = 0; i < n; i++){
+            val *= mid;
+            if(val > m) break;
+        }
+        if(val==m) return mid;
+        else if(val>m)end=mid-1;
+        else st=mid+1;
+    }
+    return -1;
+}
+
 int main(){
     int x=871765;
     int sqrt=mySqrt(x);
