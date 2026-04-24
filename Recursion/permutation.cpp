280291191ae0de6155a8ef468de4745b615ecd65
Susovan Paul <palsusovan88@gmail.com>
2026-04-24 11:00:49 +0530

    solved get premutation of a string

--- Recursion/permutation.cpp
+++ Recursion/permutation.cpp
@@ -4,6 +4,32 @@
 #include <algorithm>
 using namespace std;
 
+// https://www.geeksforgeeks.org/problems/permutations-of-a-given-string-1587115620/1
+vector<string>ans;
+int n;
+void helper(string s,string &temp){
+    if(temp.size()==n){
+        ans.push_back(temp);
+        // cout<<temp<<endl;
+        return;
+    }
+    for(int i=0;i<s.size();i++){
+        temp.push_back(s[i]);
+        string rem=s.substr(0,i)+s.substr(i+1);
+        helper(rem,temp);
+        temp.pop_back();
+    }
+    return;
+}
+vector<string> permutation(string S) {
+    // Your code here
+    n=S.size();
+    string temp;
+    helper(S,temp);
+    sort(ans.begin(),ans.end());
+    return ans;
+}
+
 void permutationOfStr(string ans,string str){
     if(str==""){
         cout<<ans<<" ";
@@ -18,5 +44,7 @@ void permutationOfStr(string ans,string str){
 
 int main(){
     string str="abcd";
-    permutationOfStr("",str);
+    // permutationOfStr("",str);
+    permutation(str);
+    for(auto ele : ans)cout<<ele<<endl;
 }
